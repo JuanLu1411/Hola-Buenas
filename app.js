4bf18cf0213646a379e4c7e7ddad9e2492e3e21a
JuanLu1411 <94109552+JuanLu1411@users.noreply.github.com>
2022-10-06 06:39:10 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 document.body.style.backgroundColor = '#7FFFD4';
 document.body.style.fontFamily = 'Arial';
-document.body.style.backgroundfo = 'red';
+document.body.style.color = "red";
+document.body.style.border="10p".borderColor= "red";
+
+window.onload = function() {
+    setInterval(cambiarFondo, 30000);
+    }
+    fondoActual = 0;
+    imagenes = ["Fondo1.gif", "Fondo2.gif"];
+    function cambiarFondo() {
+    document.body.style.backgroundImage = "url(" + imagenes[++fondoActual % imagenes.length] + ")";
+}
+
+document.Parrafos.style.color = "Blue";
+
