caaecd82376bca6c83954c6676edfa3741b27124
JuanLu1411 <94109552+JuanLu1411@users.noreply.github.com>
2022-10-06 10:02:40 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 document.body.style.backgroundColor = '#7FFFD4';
 document.body.style.fontFamily = 'Arial';
 document.body.style.color = "red";
-document.body.style.border="10p".borderColor= "red";
+document.body.style.border="3px solid red";
 
 window.onload = function() {
-    setInterval(cambiarFondo, 30000);
+    setInterval(cambiarFondo, 5000);
     }
     fondoActual = 0;
     imagenes = ["Fondo1.gif", "Fondo2.gif"];
@@ -12,5 +12,29 @@ window.onload = function() {
     document.body.style.backgroundImage = "url(" + imagenes[++fondoActual % imagenes.length] + ")";
 }
 
-document.Parrafos.style.color = "Blue";
+var Parrafos = document.body.getElementsByTagName("h1")[0];{
+Parrafos.style.color="blue";
+};
 
+var lista = document.body.getElementsByTagName("div")[1];
+
+var listita = lista.lastElementChild;
+
+for (i=4; i<7; i++){
+
+const node = document.createElement("li");
+
+const textnode = document.createTextNode("object "+i);
+
+node.appendChild(textnode);
+
+listita.appendChild(node);
+
+}
+
+
+
+
+//var Parrafos = document.getElementById("Parrafos");{
+//    Parrafos.style.color="blue";
+//}
