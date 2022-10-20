a17c5446e16c855ebcf207cc9797f67fbf82f853
JuanLu1411 <94109552+JuanLu1411@users.noreply.github.com>
2022-10-20 14:19:30 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,40 +1,57 @@
-document.body.style.backgroundColor = '#7FFFD4';
-document.body.style.fontFamily = 'Arial';
-document.body.style.color = "red";
-document.body.style.border="3px solid red";
-
-window.onload = function() {
-    setInterval(cambiarFondo, 5000);
-    }
-    fondoActual = 0;
-    imagenes = ["Fondo1.gif", "Fondo2.gif"];
-    function cambiarFondo() {
-    document.body.style.backgroundImage = "url(" + imagenes[++fondoActual % imagenes.length] + ")";
-}
-
-var Parrafos = document.body.getElementsByTagName("h1")[0];{
-Parrafos.style.color="blue";
-};
-
-var lista = document.body.getElementsByTagName("div")[1];
-
-var listita = lista.lastElementChild;
-
-for (i=4; i<7; i++){
-
-const node = document.createElement("li");
-
-const textnode = document.createTextNode("object "+i);
-
-node.appendChild(textnode);
-
-listita.appendChild(node);
-
-}
+/* Esto esta muy vacio */
+/*Ejercicio 1 */
+var asignacion = document.body.getElementsByTagName("div")[1];
+asignacion.setAttribute("divContenido","divIzquierdo");
+var asignacion2 = asignacion.nextElementSibling;
+asignacion2.setAttribute("divContenido","divCentro");
+var asignacion3 = asignacion2.nextElementSibling;
+asignacion3.setAttribute("divContenido","divDerecho");
 
 
+/*Ejercicio 2*/
+asignacion.style.backgroundColor= "red";
+asignacion.style.margin = "7px";
 
 
-//var Parrafos = document.getElementById("Parrafos");{
-//    Parrafos.style.color="blue";
-//}
+asignacion2.style.backgroundColor = "orange";
+asignacion2.style.margin = "7px";
+
+
+asignacion3.style.backgroundColor= "white";
+asignacion2.style.margin = "7px";
+
+/*Ejercicio 3*/
+asignacion.style.width = "30vw";
+
+asignacion2.style.width = "30vw";
+
+asignacion3.style.width = "30vw";
+
+/*Ejercicio 4*/
+asignacion.style.display = "inline-block";
+asignacion.style.border="3px solid ";
+
+asignacion2.style.display = "inline-block";
+asignacion2.style.border="3px solid";
+
+asignacion3.style.display = "inline-block";
+asignacion3.style.border="3px solid";
+
+/*Ejercicio 5*/
+
+var contenedor = document.body.getElementsByTagName("div")[0];
+
+contenedor.style.height = "75vh";
+/*contenedor.style.display = "flex";*/
+
+/*Ejercicio 6*/
+asignacion.style.height = "80%";
+
+asignacion2.style.height = "80%";
+
+asignacion3.style.height = "80%";
+
+var PIE = document.body.getElementsByTagName("div")[4];
+PIE.style.height = "5%";
+PIE.style.border="3px solid";
+
